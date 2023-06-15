5797f69eb8b1c2f976cb5882f7e64ad6ec0a8715
Zoe Gough <zoe.gough@papercut.com>
2023-06-15 18:08:27 +1000

    chore: erzodox fixes

--- keyboards/ergodox_ez/keymaps/erzodox/keymap.c
+++ keyboards/ergodox_ez/keymaps/erzodox/keymap.c
@@ -20,7 +20,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_WIN_BASE] = LAYOUT_ergodox_pretty(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TO(_WIN_BASE),    TO(_FN), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TO(_WIN_BASE),    TO(_AK), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LWIN, KC_LALT, TT(_FN), TO(_WIN_BASE),                                        KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, SWAP_OS,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_MAC_BASE] = LAYOUT_ergodox_pretty(
         QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TO(_MAC_BASE),    TO(_FN), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TO(_MAC_BASE),    TO(_AK), KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC,          KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_LOPT, KC_LCMD, MO(_FN), TO(_MAC_BASE),                                        KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, SWAP_OS,
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,                                              _______, _______, _______, _______, _______,
                                                      _______, _______,          _______, _______,
                                                               _______,          _______,
-                                            _______, _______, _______,           _______, _______, _______
+                                            _______, _______, _______,          _______, _______, _______
     ),
     [_MK] = LAYOUT_ergodox_pretty(
         _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______,                                              KC_MPRV, KC_VOLU, KC_VOLD, KC_MNXT, _______,
                                                      _______, _______,          _______, _______,
                                                               _______,          _______,
-                                            _______, _______, _______,           _______, _______, _______
+                                            _______, _______, _______,          _______, _______, _______
     ),
     [_NP] = LAYOUT_ergodox_pretty(
         _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, _______,
